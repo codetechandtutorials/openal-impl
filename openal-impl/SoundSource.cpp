4d2a04bb0f0394a3fe42b7eb099f0b7051791415
Matthew Early <matthewjearly@gmail.com>
2020-11-09 05:59:19 -0500

    remove unnecessary vars

--- openal-impl/SoundSource.cpp
+++ openal-impl/SoundSource.cpp
@@ -4,11 +4,6 @@
 SoundSource::SoundSource()
 {
 	alGenSources(1, &p_Source);
-	alSourcef(p_Source, AL_PITCH, p_Pitch);
-	alSourcef(p_Source, AL_GAIN, p_Gain);
-	alSource3f(p_Source, AL_POSITION, p_Position[0], p_Position[1], p_Position[2]);
-	alSource3f(p_Source, AL_VELOCITY, p_Velocity[0], p_Velocity[1], p_Velocity[2]);
-	alSourcei(p_Source, AL_LOOPING, p_LoopSound);
 	alSourcei(p_Source, AL_BUFFER, p_Buffer);
 }
 
--- openal-impl/SoundSource.h
+++ openal-impl/SoundSource.h
@@ -10,11 +10,6 @@ public:
 
 private:
 	ALuint p_Source;
-	float p_Pitch = 1.f;
-	float p_Gain = 1.f;
-	float p_Position[3] = { 0,0,0 };
-	float p_Velocity[3] = { 0,0,0 };
-	bool p_LoopSound = false;
 	ALuint p_Buffer = 0;
 };
 
